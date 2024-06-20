732e3b0cc6a54547d5c7a0e28bed2c04b4f9a86c
BentoTPG <130327391+BentoTPG@users.noreply.github.com>
2024-06-20 19:26:27 +0700

    1

--- ApiServer/server.js
+++ ApiServer/server.js
@@ -1,11 +1,28 @@
-var express = require('express')
-var cors = require('cors')
+import express from 'express';
+import cors from 'cors';
 var app = express()
 
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:'12345678',
+  database: 'travel',
+});
 app.use(cors())
 
-app.get('/api/attractions', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+app.get('/api/attractions', async function (req, res, next) {
+  try {
+    const [results] = await connection.query(
+      'SELECT * FROM attraction'
+    );
+    console.log(results); // results contains rows returned by server
+    res.json(results)
+  } catch (err) {
+    console.log(err);
+  }
+  
 })
 
 app.listen(5000, function () {
