2fcb281da5e05b62640b2037b5b6537cbb2c610f
ploum00123 <158266564+ploum00123@users.noreply.github.com>
2024-06-24 21:55:38 +0700

    as

--- ApiServer/server.js
+++ ApiServer/server.js
@@ -2,28 +2,28 @@ import express from 'express';
 import cors from 'cors';
 var app = express()
 
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 
-const connection = await mysql.createConnection({
+// Your other imports and code...
+
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password:'12345678',
-  database: 'travel',
+  password: '',
+  database: 'food_database'
 });
+
 app.use(cors())
 
-app.get('/api/attractions', async function (req, res, next) {
-  try {
-    const [results] = await connection.query(
-      'SELECT * FROM attraction'
-    );
-    console.log(results); // results contains rows returned by server
-    res.json(results)
-  } catch (err) {
-    console.log(err);
-  }
-  
-})
+app.get('/api/food_database', function (req, res, next) {
+  connection.query(
+    'SELECT * FROM menus',
+    function(err, results, fields) {
+      res.json(results);
+    }
+  );
+});
+
 
 app.listen(5000, function () {
   console.log('CORS-enabled web server listening on port 5000')
