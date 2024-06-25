a8fbb7721f50dd1a905eef91ae84b7eec7b7235a
ploum00123 <158266564+ploum00123@users.noreply.github.com>
2024-06-25 22:39:18 +0700

    can connect api

--- rnfullstack/screens/Home.js
+++ rnfullstack/screens/Home.js
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://10.0.2.2:5000/api/food_database')
+      .then(res => res.json())
+      .then((result) => {
+        setItems(result);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.menu_name}</Text>
+    </View>
+  );
+
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+       data={items}
+       renderItem={renderItem}
+       keyExtractor={item => item.menu_id}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
