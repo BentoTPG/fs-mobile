dc10acf0007609467e848f68e30e50380f8f2eb7
ploum00123 <158266564+ploum00123@users.noreply.github.com>
2024-07-02 22:07:26 +0700

    detail

--- rnfullstack/api.js
+++ rnfullstack/api.js
@@ -1,25 +1,26 @@
-export const fetchIngredients = async () => {
-  const response = await fetch('http://10.0.2.2:5000/api/ingredients');
-  return response.json();
-};
-
-export const addIngredient = async (ingredient) => {
-  const response = await fetch('http://10.0.2.2:5000/api/ingredients', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name: ingredient.name }),
-  });
-  return response.json();
-};
-
 export const fetchRecipes = async () => {
   const response = await fetch('http://10.0.2.2:5000/api/food_database');
-  return response.json();
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Failed to fetch recipes');
+  }
 };
 
 export const fetchMenuIngredients = async (menu_id) => {
   const response = await fetch(`http://10.0.2.2:5000/api/food_database/${menu_id}/ingredients`);
-  return response.json();
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Failed to fetch ingredients');
+  }
+};
+
+export const fetchMenuById = async (menu_id) => {
+  const response = await fetch(`http://10.0.2.2:5000/api/food_database/${menu_id}`);
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Failed to fetch menu');
+  }
 };
